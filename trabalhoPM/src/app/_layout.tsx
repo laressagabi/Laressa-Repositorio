8d7ae8991729d3bff4786dc252be0d21acc52d62
LareGabi <167236664+laressagabi@users.noreply.github.com>
2026-06-10 14:39:57 +0000

    aula sobre navegação em telas 10/06

--- trabalhoPM/src/app/_layout.tsx
+++ trabalhoPM/src/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { ScreenStack } from "react-native-screens";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{title: 'Home'}} />
+      <Stack.Screen name="detalhes" options={{title: 'Detalhes'}} />
+    </Stack>
+  );
 }
